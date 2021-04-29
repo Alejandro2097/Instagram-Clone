a492db452ea5b57bc166333e2f5c447f3810790f
Alejandro Huertas <alejo12381@hotmail.es>
2021-04-29 14:40:10 -0500

    seed.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+//Client side rendered app: react (cra)
+    // -> database which is Firebase
+    // -> react-loading-skeleton 
+    // -> tailwind
+
+//Architecture-folder structure
+    //src
+        //-> components,
+        //-> constans,
+        //-> context,
+        //-> Helpers,
+        //-> Hooks
+        //-> pages
+        //-> lib (Firebase is going to live here)
+        //-> services (Firebase functions in here)
+        //-> styles (talwind's folder (app/talwind))
